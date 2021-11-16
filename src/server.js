1636c25242f45516dfdc58e916399575a49e50f8
James Heggs <james.heggs@techreturners.com>
2021-11-16 19:56:40 +0000

    Re-locate dummy data population

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const app = require('./app');
+const populateDummyData = require('./database/database_seed');
+
+const environment = process.env.NODE_ENV || 'dev';
 
 app.listen(3000, () => {
   console.log('Express started on port 3000');
+
+  // Seed the database with some data
+  if (environment === 'dev') {
+    populateDummyData();
+  }
 });
